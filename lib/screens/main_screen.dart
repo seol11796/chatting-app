4edd8e3280197bbd9501292a99f9ca1367fbf5b1
seol <seol11796@gmail.com>
2022-03-21 13:12:48 +0900

    second commit

--- lib/screens/main_screen.dart
+++ lib/screens/main_screen.dart
@@ -28,15 +28,167 @@ class _LoginSignupScreenState extends State<LoginSignupScreen> {
             image: DecorationImage(
               image: AssetImage('image/red.jpg'),
               fit: BoxFit.fill
-            )
-          ),
+            ),
         ),
+        child: Container(
+          padding: EdgeInsets.only(top:90,left: 20),
+         child: Column(
+           crossAxisAlignment: CrossAxisAlignment.start ,
+          children: [
+            RichText(
+              text:TextSpan(
+                text: 'Welcome',
+                style: TextStyle(
+                  letterSpacing: 1.0,
+                  fontSize: 25,
+                  color:Colors.white
+                ),
+                children: [
+                  TextSpan(
+                  text: ' to Yummy chat! ',
+                  style: TextStyle(
+                      letterSpacing: 1.0,
+                      fontSize: 25,
+                      color:Colors.white,
+                    fontWeight: FontWeight.bold
+                  ),
+                  )
+                ]
+              ),
+            ),
+            SizedBox(height: 5.0,
+            ),
+            Text(
+                'signup to continue',
+                style: TextStyle(
+                    letterSpacing: 1.0,
+                    color:Colors.white,
+                ),
+            ),
 
+          ],
+         ),
+        )
+        ),
+      ),
+      Positioned(
+        top: 180,
+          child: Container(
+
+            padding: EdgeInsets.all(20.0),
+            height: 280.0,
+            width:MediaQuery.of(context).size.width-40,
+            margin: EdgeInsets.symmetric(horizontal: 20.0),
+            decoration: BoxDecoration(
+              color: Colors.white,
+              borderRadius: BorderRadius.circular(15.0),
+              boxShadow: [
+                BoxShadow(
+                  color: Colors.black.withOpacity(0.3),
+                  blurRadius: 15,
+                  spreadRadius: 5
+                )
+              ],
+            ),
+            child: Column(
+              children: [
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceAround,
+                  children: [
+                    GestureDetector(
+                      onTap: (){
+                        setState(() {
+                        isSignupScreen = false;
+                        });
+                      },
+                      child: Column(
+                        children: [
+                          Text(
+                            'LOGIN',
+                            style: TextStyle(
+                              fontSize: 16,
+                              fontWeight: FontWeight.bold,
+                              color:!isSignupScreen? Palette.activeColor
+                                  : Palette.textColor1
+                            ),
+                          ),
+                          if(!isSignupScreen)
+                          Container(
+                            margin: EdgeInsets.only(top: 3),
+                            height: 2,
+                            width: 55,
+                            color:Colors.orange
+                          )
+                        ],
+                      ),
+                    ),
+                    GestureDetector(
+                      onTap: (){
+                        setState(() {
+                          isSignupScreen = true;
+                        });
+                      },
+                      child: Column(
+                        children: [
+                          Text(
+                            'SIGNUP',
+                            style: TextStyle(
+                                fontSize: 16,
+                                fontWeight: FontWeight.bold,
+                                color: isSignupScreen? Palette.activeColor :
+                                    Palette.textColor1
+                            ),
+                          ),
+                          if(isSignupScreen)
+                          Container(
+                            margin: EdgeInsets.only(top:3),
+                              height: 2,
+                              width: 55,
+                              color:Colors.orange
+                          )
+                        ],
+                      ),
+                    )
+                  ],
+                ),
+                Container(
+                  child:Form(
+                    child: Column(
+                      children: [
+                        TextFormField(
+                          decoration: InputDecoration(
+                          prefixIcon: Icon(Icons.account_circle,
+                          color: Palette.iconColor,
+                          ),
+                            enabledBorder: OutlineInputBorder(
+                              borderSide: BorderSide(
+                                color: Palette.textColor1
+                              ),
+                              borderRadius: BorderRadius.all(
+                                  Radius.circular(35.0))
+                            ),
+                            focusedBorder: OutlineInputBorder(
+                                borderSide: BorderSide(
+                                    color: Palette.textColor1
+                                ),
+                                borderRadius: BorderRadius.all(
+                                    Radius.circular(35.0))
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                  )
+                ),
+              ],
+            ),
+          ),
       ),
     ],
       ),
     );
   }
+
 }
 
 
