d2234fd12cd0ae0fdadfed1e3b7fae20ba5c621b
seol <seol11796@gmail.com>
2022-03-22 11:02:42 +0900

    third commit

--- lib/screens/main_screen.dart
+++ lib/screens/main_screen.dart
@@ -18,6 +18,7 @@ class _LoginSignupScreenState extends State<LoginSignupScreen> {
       backgroundColor: Palette.backgroundColor,
       body: Stack(
     children: [
+      // 배경
       Positioned(
         top: 0,
         right: 0,
@@ -71,6 +72,7 @@ class _LoginSignupScreenState extends State<LoginSignupScreen> {
         )
         ),
       ),
+      // 텍스트 폼 필드
       Positioned(
         top: 180,
           child: Container(
@@ -151,7 +153,9 @@ class _LoginSignupScreenState extends State<LoginSignupScreen> {
                     )
                   ],
                 ),
+
                 Container(
+                  margin: EdgeInsets.only(top:20),
                   child:Form(
                     child: Column(
                       children: [
@@ -174,6 +178,72 @@ class _LoginSignupScreenState extends State<LoginSignupScreen> {
                                 borderRadius: BorderRadius.all(
                                     Radius.circular(35.0))
                             ),
+                            hintText:'User name',
+                            hintStyle: TextStyle(
+                              fontSize: 14,
+                              color: Palette.textColor1
+                            ),
+                            contentPadding: EdgeInsets.all(10)
+                          ),
+                        ),
+                        SizedBox(
+                          height: 8,
+                        ),
+                        TextFormField(
+                          decoration: InputDecoration(
+                              prefixIcon: Icon(Icons.account_circle,
+                                color: Palette.iconColor,
+                              ),
+                              enabledBorder: OutlineInputBorder(
+                                  borderSide: BorderSide(
+                                      color: Palette.textColor1
+                                  ),
+                                  borderRadius: BorderRadius.all(
+                                      Radius.circular(35.0))
+                              ),
+                              focusedBorder: OutlineInputBorder(
+                                  borderSide: BorderSide(
+                                      color: Palette.textColor1
+                                  ),
+                                  borderRadius: BorderRadius.all(
+                                      Radius.circular(35.0))
+                              ),
+                              hintText:'User name',
+                              hintStyle: TextStyle(
+                                  fontSize: 14,
+                                  color: Palette.textColor1
+                              ),
+                              contentPadding: EdgeInsets.all(10)
+                          ),
+                        ),
+                        SizedBox(
+                          height: 8,
+                        ),
+                        TextFormField(
+                          decoration: InputDecoration(
+                              prefixIcon: Icon(Icons.account_circle,
+                                color: Palette.iconColor,
+                              ),
+                              enabledBorder: OutlineInputBorder(
+                                  borderSide: BorderSide(
+                                      color: Palette.textColor1
+                                  ),
+                                  borderRadius: BorderRadius.all(
+                                      Radius.circular(35.0))
+                              ),
+                              focusedBorder: OutlineInputBorder(
+                                  borderSide: BorderSide(
+                                      color: Palette.textColor1
+                                  ),
+                                  borderRadius: BorderRadius.all(
+                                      Radius.circular(35.0))
+                              ),
+                              hintText:'User name',
+                              hintStyle: TextStyle(
+                                  fontSize: 14,
+                                  color: Palette.textColor1
+                              ),
+                              contentPadding: EdgeInsets.all(10)
                           ),
                         ),
                       ],
@@ -184,7 +254,78 @@ class _LoginSignupScreenState extends State<LoginSignupScreen> {
             ),
           ),
       ),
+      // 전송 버튼
+      Positioned(
+          top: 430,
+          left: 0,
+          right: 0,
+          child:  Center(
+            child: Container(
+              padding: EdgeInsets.all(15),
+              height: 90,
+              width: 90,
+        decoration: BoxDecoration(
+            color: Colors.white,
+            borderRadius: BorderRadius.circular(50)
+        ),
+        child: Container(
+          decoration: BoxDecoration(
+            gradient: LinearGradient(
+              colors: [
+                Colors.orange,
+                Colors.red
+              ],
+              begin: Alignment.topLeft,
+              end: Alignment.bottomRight
+            ),
+            borderRadius: BorderRadius.circular(30),
+            boxShadow: [
+              BoxShadow(
+                color: Colors.black.withOpacity(0.3),
+                spreadRadius: 1,
+                blurRadius: 1,
+              offset: Offset(0,1),
+              ),
+            ],
+          ),
+
+          child: Icon(
+            Icons.arrow_forward,
+            color: Colors.white,
+
+          ),
+        ),
+      ),
+          )),
+
+      Positioned(
+          top: MediaQuery.of(context).size.height - 125,
+          right: 0,
+          left: 0,
+
+          child: Column(
+            children: [
+              Text('or Signup with'),
+              SizedBox(
+                height: 10,
+              ),
+              TextButton.icon(
+                  onPressed: (){},
+                  style:TextButton.styleFrom(
+                    primary: Colors.white,
+                    minimumSize: Size(155, 40),
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(20)
+                    ),
+                 backgroundColor: Palette.googleColor
+                  ),
+                  icon: Icon(Icons.add) ,
+                  label: Text('Google'),
+              )
+            ],
+          ))
     ],
+       
       ),
     );
   }
